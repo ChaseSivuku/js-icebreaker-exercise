9d73692be5fd16e04ae4b67f6a4006650d56ede4
nkoebotse <elliot.sekgobela@gmail.com>
2025-08-08 11:43:05 +0200

    Add partner 1 variables

---
+++ icebreaker.js
@@ -0,0 +1,10 @@
+const faveMovie = "The Croods";
+const numbSiblings = 3;
+const seenInception = false;
+const vacationSpot = "Germany, Munich. To visit and drive on the autobahn";
+
+
+console.log("Favorite Movie: ", faveMovie, ": Is a String");
+console.log("Number of siblings:" ,numbSiblings, ": Is an integer");
+console.log("Seen Inception: " ,seenInception, ": Is a boolean");
+console.log("The vacation spot: " ,vacationSpot, ": Is a String");
