04fbff3afaffb00ab8d5dfcb284d9a847529f97c
Musiki8213 <sithomolamusiki@gmail.com>
2025-08-08 11:53:38 +0200

    Add Partner 2 variables

--- icebreaker.js
+++ icebreaker.js
@@ -1,3 +1,4 @@
+/* Partner 1 */
 const faveMovie = "The Croods";
 const numbSiblings = 3;
 const seenInception = false;
@@ -8,3 +9,15 @@ console.log("Favorite Movie: ", faveMovie, ": Is a String");
 console.log("Number of siblings:" ,numbSiblings, ": Is an integer");
 console.log("Seen Inception: " ,seenInception, ": Is a boolean");
 console.log("The vacation spot: " ,vacationSpot, ": Is a String");
+
+/* Partner 2 */
+
+ const listFood = ["Pap", "Achar", "Chicken"];
+ let discountCode = null
+ let profileUrl = "";
+ const bigNumber = 1.999999999999999533333333333333334567873223456543;
+
+ console.log("List of Top Foods: ", listFood, ": Is an array");
+console.log("Empty Discount Code:" ,discountCode, ": Is null");
+console.log("Profile URL: " ,profileUrl, ": Is an empty String");
+console.log("Large number: " ,bigNumber, ": Is Float");
