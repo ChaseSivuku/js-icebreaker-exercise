94e5b994aeb91c903826cc2c383de851303edc6e
nkoebotse <elliot.sekgobela@gmail.com>
2025-08-08 13:27:23 +0200

    Added a new quest file

---
+++ quest-log.js
@@ -0,0 +1,23 @@
+questLog = ["Collect 10 Firefly wings", 
+    "Defeat the goblin king", 
+    "find the lost sword of oakhaven"];
+
+console.log(questLog);
+console.log("The hero has ", questLog.length, "quests.");
+console.log("The hero's first quest is to ",questLog[1]);
+
+questLog.push("Deliver the Royal Scroll");
+
+console.log(questLog);
+
+questLog.pop();
+
+questLog.unshift("Rescue the Blacksmith's cat");
+
+console.log(questLog);
+
+questLog.shift();
+
+console.log(questLog);
+
+
