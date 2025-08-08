5621aeaed358c9e03b0da0708fbf75823ed06a53
Musiki8213 <sithomolamusiki@gmail.com>
2025-08-08 13:44:14 +0200

    completed the tasks

--- quest-log.js
+++ quest-log.js
@@ -1,10 +1,10 @@
 questLog = ["Collect 10 Firefly wings", 
     "Defeat the goblin king", 
-    "find the lost sword of oakhaven"];
+    "Find the lost sword of oakhaven"];
 
 console.log(questLog);
 console.log("The hero has ", questLog.length, "quests.");
-console.log("The hero's first quest is to ",questLog[1]);
+console.log("The hero's first quest is to ",questLog[0]);
 
 questLog.push("Deliver the Royal Scroll");
 
@@ -20,4 +20,15 @@ questLog.shift();
 
 console.log(questLog);
 
+questLog.splice(1, 1)
 
+console.log(questLog);
+
+
+questLog.splice(1, 0,"Slay the Dragon of Mount Doom");
+
+console.log(questLog);
+
+let joinn = questLog.join("|")
+
+console.log(joinn);
